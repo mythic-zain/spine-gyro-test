3493b2ba92f6af7a8fc9ba17107d6ee03f43cb79
mythic-zain <zain@mythic.xyz>
2023-04-06 10:51:42 +0700

    Horizontal gyro testing

--- src/App.tsx
+++ src/App.tsx
@@ -9,10 +9,10 @@ function App() {
 
   	return (
     	<>
-    		{/* <WhimsyBearMoving /> */}
+    		<WhimsyBearMoving />
 			{/* <WhimsyBearStory /> */}
-			<GyroscopeSensor />
-			<GyroMotion />
+			{/* <GyroscopeSensor /> */}
+			{/* <GyroMotion /> */}
     	</>
   	);
 }
--- src/Spine/UseSpineAnimation.ts
+++ src/Spine/UseSpineAnimation.ts
@@ -13,20 +13,36 @@ interface ICursorPosition {
     y: number,
 }
 
+interface IOrientationCoordinate {
+    vAxis: number,
+    hAxis: number
+}
+
 const UseSpineAnimation = (canvasRef: any, spineUrl: string, skeletonScale = 1) => {
     const [bearSpine, setBearSpine] = useState<Spine>()
     const scaleModifier = skeletonScale === 1 ? skeletonScale : 0.8;
+    const [initialOrientationData, setInitialOrientationData] = useState<IOrientationCoordinate>();
+    const [pixiApp, setPixiApp] = useState<Application>()
+    const [bone, setBone] = useState<IBoneWithCoordinate>()
 
     let app: Application
     let container: Container
     let targetBone: IBoneWithCoordinate
 
     const moveSpine = (position: ICursorPosition) => {
+        console.log('pos', position)
         const maxX = 958;
-        const newX = position.x - app.screen.width / 2;
-        const currentX = targetBone.x;
-        targetBone.x = newX < -maxX || newX > maxX ? currentX : newX;
-        targetBone.y = -position.y + app.screen.height;
+        // const newX = position.x - app.screen.width / 2;
+        // const currentX = targetBone.x;
+        // targetBone.x = newX < -maxX || newX > maxX ? currentX : newX;
+        // targetBone.y = -position.y + app.screen.height;
+        // console.log('pos', position, targetBone.x, targetBone.y)
+
+        const newX = position.x - pixiApp!.screen.width / 2;
+        const currentX = bone!.x;
+        bone!.x = newX < -maxX || newX > maxX ? currentX : newX;
+        // bone!.y = -position.y + app.screen.height;
+        // console.log('pos', position, targetBone.x, targetBone.y)
     };
 
     const resizeContainer = () => {
@@ -43,6 +59,30 @@ const UseSpineAnimation = (canvasRef: any, spineUrl: string, skeletonScale = 1) 
         );
     };
 
+    const handleOrientation = (e: DeviceOrientationEvent) => {
+        if (!initialOrientationData) {
+            setInitialOrientationData({
+                vAxis: e.beta || 0,
+                hAxis: e.gamma || 0
+            })
+        } else {
+            const axisModifier = 10
+            const currVAxis = e.beta || 0
+            const currHAxis = e.gamma || 0
+            const newVAxis = currVAxis - initialOrientationData.vAxis
+            const newHAxis = currHAxis - initialOrientationData.hAxis
+
+            let xPos = newHAxis * axisModifier
+            if (pixiApp && xPos >= 0) xPos += pixiApp.screen.width
+
+            const position = { x: xPos, y: 0 }
+            moveSpine(position)
+
+            console.log('initial', initialOrientationData)
+            console.log('new', newVAxis, newHAxis)
+        }
+    }
+
     const onAssetsLoaded = (loader: Loader, res: any) => {
         const spine = new Spine(res.whimsyBear.spineData)
         container = SpineContainer(spine, app, scaleModifier);
@@ -50,10 +90,10 @@ const UseSpineAnimation = (canvasRef: any, spineUrl: string, skeletonScale = 1) 
 
         targetBone = spine.skeleton.findBone("AAA_Target") as IBoneWithCoordinate
 
-        app.stage.on('mousemove', (e) => {    
-            const position = { x: e.data.global.x, y: e.data.global.y }
-            moveSpine(position)
-        })
+        // app.stage.on('mousemove', (e) => {    
+        //     const position = { x: e.data.global.x, y: e.data.global.y }
+        //     moveSpine(position)
+        // })
 
         app.stage.on('touchmove', (e) => {
             const position = { x: e.data.global.x, y: e.data.global.y }
@@ -69,6 +109,8 @@ const UseSpineAnimation = (canvasRef: any, spineUrl: string, skeletonScale = 1) 
             resizeContainer();
         };
 
+        setPixiApp(app)
+        setBone(targetBone)
         setBearSpine(spine)
     }
 
@@ -88,13 +130,25 @@ const UseSpineAnimation = (canvasRef: any, spineUrl: string, skeletonScale = 1) 
             canvasRef.current.removeChild(canvasRef.current.children[0])
         }
 
-        console.log('view', app.view)
         canvasRef.current.appendChild(app.view)
 
         return () => {
             app.destroy();
         };
     }, [canvasRef]);
+
+    useEffect(() => {
+        if (!bearSpine) return;
+
+        if (window.DeviceOrientationEvent) {
+          window.addEventListener("deviceorientation", handleOrientation);
+          return () => {
+            window.removeEventListener("deviceorientation", handleOrientation);
+          };
+        } else {
+          console.log("Device orientation not supported.");
+        }
+    }, [bearSpine, initialOrientationData])
 
     return bearSpine
 } 
